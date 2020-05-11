7f435964863b03d44ba38cce88df5b58f491f8a1
Charlotte Pieters <charlotte.pieters@student.ucll.be>
2020-05-11 15:48:02 +0200

    created

---
+++ src/main/java/be/ucll/labo1_spamapp/Labo1SpamappApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.labo1_spamapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labo1SpamappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labo1SpamappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ucll/labo1_spamapp/Labo1SpamappApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.labo1_spamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo1SpamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
